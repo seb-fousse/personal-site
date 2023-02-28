6202fd234d82785631f1c2608005b822a3a10395
Seb Fousse <fousse.s@northeastern.edu>
2023-02-27 23:26:52 -0500

    HomeMenuItem animationDelay Update
    Updating HomeMenuItem to include animation delay.

--- components/HomeMenuItem.js
+++ components/HomeMenuItem.js
@@ -5,11 +5,21 @@ import styles from './HomeMenuItem.module.css'
 
 function HomeMenuItem({ text, href }) {
 
+    const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
     const letters = text.split("");
-    const letterItems = letters.map((letter) =>
+    const lettersAndDelay = []
+
+    for (let i = 0; i < letters.length; ++i) {
+        let rd = `${rand(-5000, 0)}ms`
+        let fd = `${i * 1000 }ms`
+        lettersAndDelay.push({letter: letters[i], randomDelay: rd, fixedDelay: fd});
+    }
+
+    const letterItems = lettersAndDelay.map((item) =>
         <span class="outer">
-            <span class="inner">
-                <span class="letter">{letter}</span>
+            <span class="inner" style={{animationDelay: item.randomDelay}}>
+                <span class="letter" style={{animationDelay: item.fixedDelay}}>{item.letter}</span>
             </span>
         </span>
     );
