fa49973a1b0db8a0066e92b4b2192f585141bd02
Seb Fousse <fousse.s@northeastern.edu>
2023-02-28 22:10:23 -0500

    CSS & Font Tweaks

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
+import { Roboto_Flex } from '@next/font/google'
+
+const robotoFlex = Roboto_Flex({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={robotoFlex.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
