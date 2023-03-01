5ebb155acb65dacbd070d3530a26ee12e9383720
Seb Fousse <fousse.s@northeastern.edu>
2023-03-01 00:01:25 -0500

    Create PortfolioItem.js

---
+++ components/PortfolioItem.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+export default function PortfolioItem({ title, desc, href }) {
+	return (
+		<div class="p-4 m-4 text-center">
+			<Link
+				href={href}
+				className="text-neutral-800 hover:underline font-bold text-xl">
+				{title}
+			</Link>
+			<p className="text-neutral-500 font-light text-base">
+				{desc}
+			</p>
+		</div>
+	);
+}
