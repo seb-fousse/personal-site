dd2d92563c99eca2ecdb7347ff159b9bf6721883
Seb Fousse <fousse.s@northeastern.edu>
2023-02-28 22:12:41 -0500

    projectList.json & portfolio items
    Updating the portfolio item component + client side json fetching to make the project list simpler.

--- components/portfolioitem.js
+++ components/portfolioitem.js
@@ -1,7 +1,18 @@
-function PortfolioItem({ title, subtitle, destination }) {
-    return (
-        
-    );
+import Link from 'next/link'
+
+function PortfolioItem({ title, desc, href }) {
+	return (
+		<div class="p-4 m-4 text-center">
+			<Link
+				href={href}
+				className="text-neutral-800 hover:underline font-bold text-xl">
+				{title}
+			</Link>
+			<p className="text-neutral-500 font-light text-base">
+				{desc}
+			</p>
+		</div>
+	);
 }
 
 export default PortfolioItem;
--- pages/projects.js
+++ pages/projects.js
@@ -1,12 +1,36 @@
-import Link from "next/link"
+import link from 'next/link'
+import Head from 'next/head'
+import PortfolioItem from '@/components/PortfolioItem'
+import useSWR from 'swr'
 
-import link from 'next/link';
+// See link for info on client side data fetching
+// https://nextjs.org/docs/basic-features/data-fetching/client-side
+const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
 export default function Projects() {
-    return( 
-        <>
-            <h1>Projects page</h1>
-            <Link href="/">go home</Link>
-        </>
-    );
+
+	// Fetching data from projectList.json
+	const { data, error, isLoading } = useSWR('/api/project-data', fetcher)
+  if (error) return <div>Failed to load</div> // maybe change this
+  if (isLoading) return <div>Loading...</div>	// maybe change this
+
+	const projectsJson = JSON.parse(data);
+
+	return (
+		<>
+			<Head>
+
+			</Head>
+
+			<main className="flex flex-col justify-between align-middle p-24 min-h-full bg-orange-100">
+				<h1>Projects page</h1>
+
+				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3">
+					{projectsJson.map((proj) => {
+						return (<PortfolioItem title={proj.title} desc={proj.desc} href={proj.href} />)
+					})}
+				</div>
+			</main>
+		</>
+	);
 }
